907c313fdea9fac715c8a32dbd6e0d67cc408541
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-23 19:21:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,8 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if position_taken == WIN_COMBINATIONS.between?(0, 7)
+  position_taken(board,index)gets.strip
+  if position_taken(board, index) == WIN_COMBINATIONS.between?(0, 7)
     return WIN_COMBINATIONS
   end
 end
