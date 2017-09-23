d9b6ec7a711720ae0f9c194a292949887e92ab64
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-23 20:03:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,11 +16,4 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  position_taken?.select do |get|
-    get = gets.strip
-    if get == "X" || "O" == WIN_COMBINATIONS
-    WIN_COMBINATIONS
-  end
-end
-false
-end
+
